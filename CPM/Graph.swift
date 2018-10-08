e8f034a317d73db0792db0c5708c6edee6f47496
Tebin <tebin.raouf@gmail.com>
2018-10-08 01:52:04 -0400

    added full comments 100%

--- CPM/Graph.swift
+++ CPM/Graph.swift
@@ -7,20 +7,50 @@
 //
 
 import Foundation
-
+/// - Author: Tebin Raouf
+///
+///The class to represent a graph
 public class Graph: Equatable {
+    ///All the tasks in the graph
     public var tasks: [TaskNode]
     
+    ///Initialize an empty graph
+    ///
+    /// `tasks` is empty
     public init() {
         self.tasks = []
     }
+    
+    ///Add a task to the graph
+    /// - Parameter task: the task to be added
+    /// - Returns: void
     public func addTask(_ task: TaskNode) {
         tasks.append(task)
     }
+    
+    ///Add a successor task to the current (source) task. This method adds an `Edge` between the two tasks.
+    /// - Parameter source: the task in which a successor is added
+    /// - Parameter neighbor: the successor task for the source task
+    /// - Returns: void
     public func addSuccessorEdge(_ source: TaskNode, neighbor: TaskNode) {
         let edge = Edge(neighbor)
         source.successors.append(edge)
     }
+    
+    ///Add a predecessor task to the current (source) task. This method adds an `Edge` between the two tasks.
+    /// - Parameter source: the task in which a predecessor is added
+    /// - Parameter neighbor: the predecessor task for the source task
+    /// - Returns: void
+    public func addPredecessorEdge(_ source: TaskNode?, neighbor: TaskNode?) {
+        if let s = source, let n = neighbor  {
+            let edge = Edge(n)
+            s.predecessors.append(edge)
+        }
+    }
+    
+    ///Get a task object by its name
+    /// - Parameter name: the name of the task
+    /// - Returns: a `TaskNode` object (if any)
     public func getTask(by name: String) -> TaskNode? {
         for task in tasks {
             if task.name == name {
@@ -29,13 +59,13 @@ public class Graph: Equatable {
         }
         return nil
     }
-    public func addPredecessorEdge(_ source: TaskNode?, neighbor: TaskNode?) {
-        if let s = source, let n = neighbor  {
-            let edge = Edge(n)
-            s.predecessors.append(edge)
-        }
-    }
+
 }
+///Compare if two graphs (graph objects) are equal
+///
+/// - Parameter lhs: Graph object one
+/// - Parameter rhs: Graph Object two
+/// - Returns: true if the two graphs are equal; false otherwise
 public func == (_ lhs: Graph, rhs: Graph) -> Bool {
     return lhs.tasks == rhs.tasks
 }
